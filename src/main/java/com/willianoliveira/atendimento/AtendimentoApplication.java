a0630b47e93583bef8829c1c007627ab5eb645fb
Willian Oliveira <wijloc@gmail.com>
2020-04-27 21:40:41 -0300

    git init

---
+++ src/main/java/com/willianoliveira/atendimento/AtendimentoApplication.java
@@ -0,0 +1,13 @@
+package com.willianoliveira.atendimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtendimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtendimentoApplication.class, args);
+	}
+
+}
