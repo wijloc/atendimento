7656a493734929e07980a6f9e2455834233b8339
Willian Oliveira <wijloc@gmail.com>
2020-05-12 16:33:56 -0300

    jpa anotations

--- src/main/java/com/willianoliveira/atendimento/model/Atendimento.java
+++ src/main/java/com/willianoliveira/atendimento/model/Atendimento.java
@@ -1,9 +1,22 @@
 package com.willianoliveira.atendimento.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@Entity
 public class Atendimento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String sumario;
     private LocalDateTime criacao;
     private String descricao;
--- src/main/java/com/willianoliveira/atendimento/model/Cliente.java
+++ src/main/java/com/willianoliveira/atendimento/model/Cliente.java
@@ -1,9 +1,20 @@
 package com.willianoliveira.atendimento.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@Entity
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private String email;
 }
