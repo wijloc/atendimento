ee870ae59a778652fd4cdc3c40c0f26cc4b451f2
Willian Oliveira <wijloc@gmail.com>
2020-05-12 16:51:30 -0300

    documentacao do Atendimento

--- src/main/java/com/willianoliveira/atendimento/model/Atendimento.java
+++ src/main/java/com/willianoliveira/atendimento/model/Atendimento.java
@@ -12,21 +12,47 @@ import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Um atendiento é uma iteração criada pelo cliente e atendidada operador
+ */
 @Data
 @NoArgsConstructor
 @Entity
 public class Atendimento {
+
+    /**
+     * Identificador do objeto
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    /**
+     * Descrição breve para que o operador possa iniciar o atendimento
+     * @see #descricao
+     */
     private String sumario;
+    /**
+     * Data e hora da criação
+     */
     private LocalDateTime criacao;
+    /**
+     * Descrição detalhada
+     */
     private String descricao;
+    /**
+     * Tipo de atendimento de acordo com as opções disponíveis em {@link com.willianoliveira.atendimento.model.TipoAtendimento Tipo de Atendimento}.
+     * @see #sumario
+     */
     @Enumerated(EnumType.STRING)
     private TipoAtendimento tipo;
+    /**
+     * {@link com.willianoliveira.atendimento.model.Cliente Cliente} que requisitou o atendimento.
+     */
     @ManyToOne
     private Cliente cliente;
+    /**
+     * {@link com.willianoliveira.atendimento.model.Operador Operador} responsável pela próxima iteração do atendimento.
+     */
     @ManyToOne
     private Operador responsavel;
 }
